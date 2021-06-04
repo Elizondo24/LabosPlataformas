0f3d7a3f1bad83ba4d7d3c3031e232fc72b0f827
Elizondo24 <brayan.jo.ev@gmail.com>
2021-06-04 15:24:59 -0600

    Code

--- scr/archivo.c
+++ scr/archivo.c
@@ -5,21 +5,18 @@ void leerArchivo(char path []){
 
     FILE *archivo;
     char cadena [10];
-    archivo = fopen(path,"rb");
+    archivo = fopen(path,"r");
 
     if (archivo != NULL)
     {
        while (!feof(archivo))
        {
            fgets(cadena, 10, archivo);
-           printf("Usuario: %s",cadena);
+
+           printf("Usuario: %s\n",cadena);
        }
        
-    } else
-    {
-        perror("Error en la apertura del archivo");
-    }
-    
+    } 
     fclose(archivo);
     
 }   
@@ -28,17 +25,25 @@ int main(){
 
     int a;
     
-    printf("0- Para salir\n");
-    printf("1-Imprimir lista de usuarios\n");
-    printf("2-Imprimir lista de usuarios y su condicion\n");
-    printf("3-Ordenar los usuarios por ultima ora de accseso\n");
-    printf("4-Actualizar los datos de un usuario\n") ;
-    printf("Digite el numero de la accion que desea realizar:");
-    scanf( "%d", &a);
     
-    while (a!=0)
+
+    while (1)
     {
-        if (a= 1)
+        printf("0- Para salir\n");
+        printf("1-Imprimir lista de usuarios\n");
+        printf("2-Imprimir lista de usuarios y su condicion\n");
+        printf("3-Ordenar los usuarios por ultima ora de accseso\n");
+        printf("4-Actualizar los datos de un usuario\n") ;
+        printf("Digite el numero de la accion que desea realizar:");
+        scanf( "%d", &a);
+
+        if (a==0)
+        {
+            return 0;
+        }
+        
+        
+        if (a==1)
         {
             leerArchivo("user_list.txt");
         }
